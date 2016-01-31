fa11b7c990571004e62b513360abc082b7da15ed
Hamid <Hamid.CCP@gmail.com>
2016-01-31 18:51:01 +0200

    added support for RGB

--- read_data.py
+++ read_data.py
@@ -7,14 +7,14 @@ n_classes = 0
 def extract_images():
     logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
     lfw_people = fetch_lfw_people(data_home= '~/Desktop/FooFoo', download_if_missing=True, 
-      resize= 1, slice_= (slice(25, 225, None), slice(25, 225, None)), color=False, min_faces_per_person=5)
+      resize= 1, slice_= (slice(25, 225, None), slice(25, 225, None)), color=True, min_faces_per_person=5)
 
     target_names = lfw_people.target_names
     global n_classes
     n_classes = target_names.shape[0]
     lfw_people_images = lfw_people.images
     print(lfw_people_images.shape)
-    lfw_people_images_resized = numpy.resize(lfw_people_images,(lfw_people_images.shape[0], 40000, ))
+    lfw_people_images_resized = numpy.resize(lfw_people_images,(lfw_people_images.shape[0], 40000, 3, ))
 
     lfw_people_target_ids = lfw_people.target
     return (lfw_people_images_resized, lfw_people_target_ids)
